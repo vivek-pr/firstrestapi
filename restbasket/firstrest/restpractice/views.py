445487b422e3b91788c998193b95526c7573ee54
vivekpradhanC <vivekpradhan.91@gmail.com>
2015-10-24 21:22:05 +0530

    first commit

---
+++ restbasket/firstrest/restpractice/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User,Group
+from rest_framework import viewsets
+from restpractice.serializers import UserSerializer,GroupSerializer
+
+# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset=User.objects.all().order_by('-date_joined')
+    serializer_class=UserSerializer
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset=Group.objects.all()
+    serializer_class=GroupSerializer
